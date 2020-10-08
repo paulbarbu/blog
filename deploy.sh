f2418086075e7a8483008c159747cd01f6e6d3d4
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2020-10-08 21:57:06 +0300

    deploy with the new theme now, not the old one

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 #hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-hugo -t vanilla-bootstrap-hugo-theme
+hugo -t mainroad
 
 # Go To Public folder
 cd public
